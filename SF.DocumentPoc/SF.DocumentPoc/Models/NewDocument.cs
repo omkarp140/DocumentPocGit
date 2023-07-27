b8dc56b39ea3439255adad034ca22792c187b2ff
Omkar PatilKarade <omkar.patilkarade@simplifai.ai>
2023-07-27 12:36:54 +0530

    implemented parallel for loop to optimize

---
+++ SF.DocumentPoc/SF.DocumentPoc/Models/NewDocument.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Concurrent;
+
+namespace SF.DocumentPoc.Models
+{
+    public class NewDocument
+    {
+        public byte[] BinaryData { get; set; }
+        public string FileName { get; set; }
+        public ConcurrentBag<TextReplacementHelperDto> TextReplacementList { get; set; }
+    }
+}
